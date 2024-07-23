4919c7df27fe0dbbeafcdf19c3b04f7daec5bb2b
jb520 <jnavarro1995@gmail.com>
2024-07-23 16:23:33 -0500

    migrate database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_23_202230) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_23_202512) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
   create_table "hobbies", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -54,6 +64,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_23_202230) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
   add_foreign_key "posts", "hobbies"
