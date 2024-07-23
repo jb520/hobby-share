65446fe726ccd1f5bf7d5460002bf4d09bcf3a42
jb520 <jnavarro1995@gmail.com>
2024-07-23 15:25:37 -0500

    create comment table

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_23_201301) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_23_202230) do
   create_table "hobbies", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -45,6 +54,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_23_201301) do
     t.index ["username"], name: "index_users_on_username", unique: true
   end
 
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "posts", "hobbies"
   add_foreign_key "posts", "users"
 end
