5205b5c680ebe4e9f61d6173be91ee772cdfafed
jb520 <jnavarro1995@gmail.com>
2024-07-25 14:12:30 -0500

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   root "users#dashboard"
-  
+
+  devise_for :users
+
   resources :comments
   resources :likes
   resources :posts
   resources :hobbies
-  devise_for :users
 
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
-  
 end
